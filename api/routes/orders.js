6f795fbaf1e7b046b467522a49629de75152f06b
Felipe Luiz Carnevali <felipe.carnevali@hotmail.com>
2018-03-18 00:49:33 +1100

    Implements moongoDb in NodeJs

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,25 +1,106 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../../models/order');
+const Product = require('../../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders were fetched!'
+    Order.find()
+           .select("product quantity _id")
+           .exec()
+           .then(orders => {
+        const response = {
+            count: orders.length,
+            orders: orders.map(order => {
+                return {
+                    quantity: order.quantity,
+                    product: order.product,
+                    _id: order._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/orders/' + order._id
+                    }
+                }
+            })
+        }
+
+        if (response.count >= 0) {
+            res.status(200).json(response);
+        } else {
+            res.status(200).json({
+                message: "No entries found"
+            });
+        } 
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
+    Product.findById(req.body.productId)
+            .then(product => {
+                if (product) {
+                    const order = new Order({
+                        _id: new mongoose.Types.ObjectId(),
+                        quantity: req.body.quantity,
+                        product: req.body.productId
+                    });
+        
+                    return order.save();
+                } else {
+                    return res.status(404).json({
+                        message: "Product not found!"
+                    });
+                }
+            })
+            .then(result => {
+                res.status(201).json({
+                    message: 'Created order successfully',
+                    createdOrder: {
+                        quantity: result.quantity,
+                        product: result.product,
+                        _id: result._id,
+                        request: {
+                            type: "GET",
+                            url: 'http://localhost:3000/orders/' + result._id
+                        }
+                    }
+                });    
+            })
+            .catch(err => {
+                console.log(err);
 
-    res.status(201).json({
-        message: 'Order was created',
-        order: order
-    });
+                res.status(500).json({
+                    error: err
+                });    
+            });
 });
 
 router.get('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+    Order.findById(id)
+            .select('product quantity _id')
+            .exec().then(order => {
+            res.status(200).json({
+                order: order,
+                request: {
+                    type: "GET",
+                    url: 'http://localhost:3000/orders'
+                }
+            });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+
     res.status(200).json({
         message: 'Order details',
         orderId: req.params.orderId
